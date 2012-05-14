96c5ed73dd381421ddf71d29bb37d3c45388833f
Guenter Klambauer <mitterecker@bioinf.jku.at>
2012-05-14 14:09:03 +0000

    Bugfix: Updated Bib entry, segplot function
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_2_10/madman/Rpacks/cn.mops@65865 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/normalizeChromosomes.R
+++ R/normalizeChromosomes.R
@@ -171,20 +171,3 @@ normalizeChromosomes <-
 	return(YY)	
 }
 
-##normalizeChromosomes(X[,1:2],ploidy=c(1,2))
-##Y <- normalizeChromosomes(X[,1:5],ploidy=c(1,2,2,2,2))
-##Y <- normalizeChromosomes(X,ploidy=c(0,rep(1,14),rep(2,25)))
-##apply(Y,2,median)
-##Y <- normalizeChromosomes(X,ploidy=c(0,rep(1,14),rep(2,25)))
-#chrIdx <- sample(c(1,2),nrow(X),replace=TRUE)
-#
-#
-#Y <- normalizeChromosomes(X,ploidy=c(0,rep(1,14),rep(2,25)),chr=chrIdx)
-#apply((Y[chrIdx==1, ]),2,median)
-#apply((Y[chrIdx==2, ]),2,median)
-#
-#
-#Y <- normalizeChromosomes(X[1:500, ],ploidy=c(rep(1,10),rep(2,30)),normType="mode")
-#r <- cn.mops(Y,norm=FALSE)
-
-
