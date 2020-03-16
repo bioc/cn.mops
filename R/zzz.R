50983e207973f075f7981c3b15148bcaa418e302
lshep <lori.shepherd@roswellpark.org>
2020-03-16 09:29:56 -0400

    User requested deprecation

--- R/zzz.R
+++ R/zzz.R
@@ -13,3 +13,9 @@
 	library.dynam.unload("cn.mops", libpath)
 }
 
+.onAttach <- function(libname, pkgname) {
+    msg <- sprintf(
+        "Package '%s' is deprecated and will be removed from Bioconductor
+         version %s", pkgname, "3.12")
+    .Deprecated(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
+}
